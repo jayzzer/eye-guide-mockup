65b6b8ad20399e6a64d6d5a211bb6424624c859f
Jayzzer <pavel10_pushkarev@mail.ru>
2017-10-17 22:25:38 +0300

    add click behaviour on search button

--- js/main.js
+++ js/main.js
@@ -4,3 +4,19 @@ var menuList = document.querySelector(menuBtn.dataset.target);
 menuBtn.onclick = function () {
     menuList.classList.toggle('in');
 }
+
+var mapSidebar = document.getElementById('map-sidebar');
+var guides = document.getElementById('guides');
+var fade = mapSidebar.querySelector('.fade');
+var searchBtn = document.querySelector('.search-submit');
+var introText = document.querySelector('.left-s__text');
+var searchForm = searchBtn.parentNode;
+var searchClasses = searchForm.className;
+var textClasses = introText.className;
+searchBtn.onclick = function () {
+    searchForm.className = searchClasses + ' bottom';
+    introText.className = textClasses + ' hidden';
+    guides.classList.remove('hidden');
+    mapSidebar.style.backgroundImage = 'url("img/map.png")';
+    fade.style.backgroundColor = '#fff';
+}
